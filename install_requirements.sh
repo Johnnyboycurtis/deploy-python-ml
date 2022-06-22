d90ae7fc0f8e0d62502f501eaaca1e045d39f040
johnnyboycurtis <johnnyboycurtis@gmail.com>
2022-06-21 19:49:28 -0700

    install the requirements

---
+++ install_requirements.sh
@@ -0,0 +1,10 @@
+conda install pip -y
+conda install -c conda-forge libgomp -y
+pip install numpy==1.22.3
+pip install pandas==1.4.2
+pip install scikit-learn==1.0.2
+pip install spacy==3.3.1
+pip install lightgbm==3.3.2
+pip install joblib==1.1.0
+pip install emoji==1.7.0
+conda install boto3
